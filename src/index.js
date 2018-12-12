abfeeb9cc8f69d303c82777acbc77c77e0fc6345
Arguello Rincon <CamiloAndres.ArguelloRincon@cencosud.com.co>
2018-12-12 09:55:03 -0500

    Obtener productos filtrados en cada click

--- src/index.js
+++ src/index.js
@@ -5,24 +5,26 @@ $(function() {
   var cont = 20;
   var numberCollection = 698;
 
-
-  // Slider
-  /*const $slider = $('.swiper-wrapper');
-
-  if ($slider.is(':empty')) {
-  	$slider.hide().parent().addClass('empty');
-  } else if ($slider.children().length > 1) {
-    $slider.bxSlider({
-      prevText: '',
-      nextText: '',
-      auto: true
-    });
-  }*/
+  getCategoriesFilters(`?map=c&fq=H:${797}`)
+    .done(filters => {
+      if(filters.CategoriesTrees.length > 0){
+        //let collectionId = window.location.search.split(":")[1].split("&O")[0];
+        let collectionId = 797;
+        let category = filters.CategoriesTrees;
+        for(let i=0;i<filters.CategoriesTrees.length;i++){
+          let childrenCat = category[i].Children; 
+          for(let j=0; j<category[i].Children.length;j++){
+            renderButtonCategory(collectionId,childrenCat[j].Name,childrenCat[j].Link); 
+          }
+        }
+      }
+    })
+    .fail(e => console.log("Hubo un error", e));
 
   var loader = $(elem.shelfLoader);
   loader.fadeIn(250);
 
-  getCollectionByNumber(numberCollection, 20);
+  getCollectionByNumber(797, 20);
 
   $(document).on("click", elem.searchListFiltersSlideControls, function() {
     let t = $(this);
@@ -51,15 +53,6 @@ $(function() {
       centerMode: true
     });
   }
-
-  $("#banner-with-popup-instructions .adaptive-image").on("click", function(e) {
-    $("#instructions-to-buy").show();
-  });
-
-  $("#instructions-to-buy button").on("click", function(e) {
-    $("#instructions-to-buy").hide();
-  });
-
   // Handle events for categories
   $(".shelf-header .search-list-filters").on("click", function(e) {
     var $element = $(e.target).closest("button");
@@ -71,9 +64,20 @@ $(function() {
       .removeClass("active");
     loader.fadeIn(250);
     $(".product-shelf").empty();
-    getCollectionByNumber(numberCollection, 20);
+    let link = $element.attr("link");
+    if($element[0].id == "view-all-products"){
+      getCollectionByNumber(797, 20);
+    }else{
+      getFilteredProducts(link,797);
+    }
+    
   });
 
+  /**
+   * 
+   * @param {Number} number - Number of Collection to render 
+   * @param {Number} quantity - Number of how many products should be rendered
+   */
   function getCollectionByNumber(number, quantity) {
     Aurora.getProductShelf(`fq=H:${number}`, 1, quantity, 20)
       .done(res => {
@@ -101,19 +105,111 @@ $(function() {
       .fail(er => console.log("error"));
   }
 
+  /**
+   * 
+   * @param {Number} filterByCategory 
+   * @param {String} iconForCategory 
+   * @param {String} descriptionCategory 
+   * @param {String} textCategory 
+   */
+  function renderButtonCategory(filterByCategory, textCategory, linkToFilter){
+    let buttonToAdd = `<button link=${linkToFilter} collection=${filterByCategory} type="button" class="search-filter"><span class="text">${textCategory}</span></button>`;
 
-  function getCategoryByCollection(){
-    
+    $(".search-list-filters").append(buttonToAdd)
   }
+  /**
+   * 
+   * @param {String} params - Query for search in Vtex API
+   * @param {*} def 
+   * @param {Number} retries 
+   */
+  function getCategoriesFilters(params, def = null, retries = 0) {
+    if (def == null) {
+      def = $.Deferred();
+    }
+
+    $.get("/api/catalog_system/pub/facets/search/categorias/" + params)
+      .done(res => {
+        def.resolve(res);
+      })
+      .fail(() => {
+        if (retries < 3) {
+          retries++;
+          self.getCategoriesFilters(params, def, retries);
+        } else {
+          def.reject();
+        }
+      });
+
+    return def.promise();
+  }
+
+  function getFilteredProducts(url, collectionId, xhr = null, retries = 0) {
+    if (!url || !collectionId)
+        return console.log("'url or collectionID' is not defined");
+
+    if (!xhr)
+        xhr = $.Deferred();
+
+    let request = $.get(url +"/"+ collectionId + "?O=OrderByPriceDESC&PS=18&map=c,c,c,productClusterIds", "", "", "html")
+        .done(res => {
+            try{
+              // Spitting html code to render it later
+              // Look for class endend with "colunas"
+              var a = res.split("colunas")[1];
+              // Look for the end of </ul> inside of product-shelf class
+              var b = a.split('<div class=\"pager bottom\"')[0];
+              // Remove 12 lasts characters
+              var c = b.slice(0, -12);
+              // Remove 2 firsts characters
+              var d = c.substr(2);
+
+              // Resolve promise
+              xhr.resolve(d);
+
+              // Render into Dom
+              $(".product-shelf").html(d);
+
+              // Close loader spinner
+              loader.fadeOut(250);
+            }catch(e){
+              $(".product-shelf").html("<p>Hay un problema cargando los productos, vuelve a intentarlo más tarde</p>");
+              setTimeout(() => {
+                getFilteredProducts(url, collectionId, xhr, retries)
+              }, 3000);
+            }
+        })
+        .fail((jqxhr) => {
+            // Abort
+            if (jqxhr.status == 0) {
+                xhr.reject();
+            } else {
+                if (retries < 3) {
+                    retries++;
+                    getFilteredProducts(url, collectionId, xhr, retries);
+                } else {
+                    xhr.reject();
+                }
+            }
+        });
+
+    let promise = xhr.promise();
+
+    promise.abort = function () {
+        request.abort();
+    }
+
+    return promise;
+}
 
   $(window).scroll(function() {
     if (
       $(window).scrollTop() + $(window).height() >
-      $(document).height() - 1000
+      $(document).height() - 1000 && $("#view-all-products").hasClass("active")
     ) {
       cont += 5;
       loader.fadeIn(250);
-      getCollectionByNumber(numberCollection, cont);
+      getCollectionByNumber(797, cont);
     }
   });
 });
